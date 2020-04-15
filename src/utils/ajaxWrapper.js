14b690e3507bec9285997d39ebdae21298bf6fff
Marsa Baniasadi <mbani83@gmail.com>
2020-04-15 10:08:35 +0200

    Change handleError to arrow function

--- src/utils/ajaxWrapper.js
+++ src/utils/ajaxWrapper.js
@@ -1,21 +1,14 @@
 import axios from 'axios';
 
 
-// const handleError = fn => (...params) => fn(...params).catch((err) => console.error('Error', err))
 
-function handleError(fn) {
-  return function(...params) {
-    return fn(...params).catch(function(err) {
-      // Handle the error here
-    })
-  }
-}
+const handleError = fn => (...params) => fn(...params).catch((err) => console.error('Error', err))
 
 const ajaxWrapper = async (url, method = "GET", body = {}) => {
     const headers = {
       "Content-Type": "application/json",
     };
-    
+
     const result = await axios({
       url,
       method,
@@ -23,7 +16,6 @@ const ajaxWrapper = async (url, method = "GET", body = {}) => {
       [method === "POST" ? "data" : "params"]: body,
     });
     return result;
-    
 };
 
 const safeAjaxWrapper = handleError(ajaxWrapper)
